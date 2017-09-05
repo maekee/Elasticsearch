3b23945e7a3eae4bb0196c89198ca6ce183e532b
Maekee <drmicke@gmail.com>
2017-09-05 11:17:53 +0200

    Create Index_commands.ps1

---
+++ Elasticsearch/Index_commands.ps1
@@ -0,0 +1,7 @@
+#Removing Index content
+
+#Removing all filebeat content from elasticsearch
+Invoke-WebRequest -Method Delete -Uri http://localhost:9200/filebeat-* -ContentType application/json
+
+#Removing all winlogbeat content from elasticsearch
+Invoke-WebRequest -Method Delete -Uri http://localhost:9200/winlogbeat-* -ContentType application/json
