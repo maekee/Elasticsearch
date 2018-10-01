8d5e1a98a83e806cc5eba1bb68a3163be2e62ae0
Maekee <drmicke@gmail.com>
2018-10-01 10:44:18 +0200

    Create PSObj-Json-PSObj.ps1

---
+++ PSObj-Json-PSObj.ps1
@@ -0,0 +1,30 @@
+#Create Json from Powershell (hashtable)
+$searchBody = @{
+  "query" = @{
+    "match_all" = @{}
+  }
+  "sort" = @{
+    "errorcode" = "desc"
+  }
+  "size" = 100
+} | ConvertTo-Json
+
+#Create PSObject from Json
+$PSObj = @'
+{
+  "query": {"match_all": {} },
+  "sort":  {"errorcode":  "desc"},
+  "size":  100
+}
+'@
+$PSObj | ConvertFrom-Json
+
+#Convert PSObject to Json
+$PSObj | ConvertTo-Json
+
+#Convert Json to PSObject and back to Json
+$searchBody | ConvertFrom-Json | ConvertTo-Json
+
+#Now you know how to use Json
+#Either by building it from PowerShell or building it from raw JSON data
+#But also how to convert it from and to with cmdlets ConvertFrom-Json and ConvertTo-Json
